f758e74fc949a58c4498824cbf36c4e46f1c8e01
Aarzoo Bansal <aarzoo1701@gmail.com>
2026-02-24 11:39:15 -0800

    Removed unwanted imports

--- client/src/components/JeopardyGame.jsx
+++ client/src/components/JeopardyGame.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useMemo } from "react";
+import { useState, useEffect } from "react";
 import ParticlesBg from './ParticlesBg'
 import SpinWheel from './SpinWheel'
 import ConfettiBurst from './ConfettiBurst'
