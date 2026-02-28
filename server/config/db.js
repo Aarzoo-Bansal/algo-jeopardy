96142aa65a59f89d85d279ad7dce5e70e9b302a8
Aarzoo Bansal <aarzoo1701@gmail.com>
2026-02-27 17:39:17 -0800

    Made changes to pool config to be compatible with Render

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+const pool = process.env.DATABASE_URL
+? new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false},
+})
+: new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
