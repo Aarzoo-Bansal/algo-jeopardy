101bcde290c8c2ce0fe48f6c8f18f042bd6a9cf4
Aarzoo Bansal <aarzoo1701@gmail.com>
2026-03-05 12:14:02 -0800

    Added test user database connection

--- server/config/db.js
+++ server/config/db.js
@@ -3,22 +3,31 @@ require('dotenv').config();
 
 const isTest = process.env.NODE_ENV === 'test';
 
-const pool = isTest ?
-    new Pool({
-        connectionString: process.env.TEST_DATABASE_URL,
-    }) :
-    (process.env.DATABASE_URL
-        ? new Pool({
-            connectionString: process.env.DATABASE_URL,
-            ssl: { rejectUnauthorized: false },
-        })
-        : new Pool({
+let pool;
+
+if (isTest) {
+    pool = new Pool({
+        user: process.env.TEST_DB_USER,
+        password: process.env.TEST_DB_PASSWORD,
+        host: process.env.TEST_DB_HOST,
+        port: process.env.TEST_DB_PORT,
+        database: process.env.TEST_DB_NAME
+    })
+
+} else if (process.env.DATABASE_URL) {
+    pool = new Pool ({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    })
+} else {
+    pool = new Pool({
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             host: process.env.DB_HOST,
             port: process.env.DB_PORT,
             database: process.env.DB_NAME
-        }));
+        });
+}
 
 // Testing connection if not a test environment
 if (!isTest) {
